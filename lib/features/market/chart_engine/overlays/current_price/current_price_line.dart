c24444f3287f97adda962263f57eaa769b42078e
Aman Ansari <amanansari966766@gmail.com>
2026-05-22 20:38:39 +0530

    scroll to latest candle done with dotted line

--- lib/features/market/chart_engine/overlays/current_price/current_price_line.dart
+++ lib/features/market/chart_engine/overlays/current_price/current_price_line.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../../core/utils/constants/app_colors.dart';
 import '../../core/models/candle_model.dart';
 import '../../core/utils/chart_math.dart';
+import '../../providers/viewport_provider.dart';
 
-class CurrentPriceLine extends StatelessWidget {
+class CurrentPriceLine extends ConsumerWidget {
+
   final List<CandleModel> candles;
 
   final double chartHeight;
@@ -26,18 +29,25 @@ class CurrentPriceLine extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,
+      WidgetRef ref,) {
+
     if (candles.isEmpty) {
       return const SizedBox();
     }
 
-    final lastCandle = candles.last;
+    final viewport =
+    ref.watch(viewportProvider);
 
-    final currentPrice = lastCandle.close;
+    final lastCandle =
+        candles.last;
 
+    final currentPrice =
+        lastCandle.close;
 
+    final y =
+    ChartMath.priceToY(
 
-    final y = ChartMath.priceToY(
       price: currentPrice,
 
       minPrice: minPrice,
@@ -47,19 +57,65 @@ class CurrentPriceLine extends StatelessWidget {
       height: chartHeight,
     );
 
-    final isBull = lastCandle.close >= lastCandle.open;
+    final isBull =
+        lastCandle.close >=
+            lastCandle.open;
+
+    final latestVisible =
+        viewport.visibleEndIndex >=
+            candles.length - 2;
 
     return Positioned(
+
       left: 0,
       right: 0,
 
       top: y,
 
       child: IgnorePointer(
-        child: Container(
+
+        child:
+
+        latestVisible
+
+            ? Container(
+
           height: 1,
 
-          color: isBull ? AppColors.green : AppColors.red,
+          color:
+          isBull
+              ? AppColors.green
+              : AppColors.red,
+        )
+
+            : Row(
+
+          children: List.generate(
+
+            80,
+
+                (index) {
+              return Expanded(
+
+                child: Container(
+
+                  height: 1,
+
+                  color:
+
+                  index.isEven
+
+                      ? (
+                      isBull
+                          ? AppColors.green
+                          : AppColors.red
+                  )
+
+                      : Colors.transparent,
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
